342448ccbf776e6215c1150f91408a41488ff389
hslee <hslee@flyhigh-x.com>
2020-08-21 15:25:31 +0900

    merchant 목록도 나옴

--- backend/merchant/merchant.route.ts
+++ backend/merchant/merchant.route.ts
@@ -5,14 +5,8 @@ import MerchantController from "./merchant.controller";
 const mer_router = express.Router();
 const mer_cont = new MerchantController();
 
-// mer_router.get('/', mer_cont.getAll)
-mer_router.get('/', (req, res) => {
-    console.log("here;;;");
-})
-            // .post('/', mer_cont.insert);
-            .post('/', (req, res) => {
-                console.log("here;;;;;;;;;;;");
-            });
+mer_router.get('/', mer_cont.getAll)
+            .post('/', mer_cont.insert);
 mer_router.get('/count', mer_cont.count);
 mer_router.get('/:id', mer_cont.get)
             .put('/:id', mer_cont.update)
