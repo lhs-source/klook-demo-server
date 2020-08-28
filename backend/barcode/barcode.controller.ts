0510de075e132eb0344c49a403f1d4923bf4cdc9
hslee <hslee@flyhigh-x.com>
2020-08-28 10:11:13 +0900

    barcode 만들어지는 것까지는 됐다. (jsbarcode 사용)

--- backend/barcode/barcode.controller.ts
+++ backend/barcode/barcode.controller.ts
@@ -1,23 +1,45 @@
-import barcode = require('barcode');
-import * as path from 'path';
+// import barcode = require('barcode');
+// import * as Barc from 'barc';
+// import * as path from 'path';
+import * as JsBarcode from 'jsbarcode';
+import { Canvas } from 'canvas';
 
 export default class BarcodeController {
     create_barcode = (req, res) =>{
-        console.log(barcode);
-        console.log(req.body.data);
-        let code39 = barcode('code39', {
-            data: req.body.data,
-            width: 400,
-            height: 100,
+        // console.dir(Barc.toString());
+        // console.log(req.body.data);
+
+        let canvas = new Canvas(400, 100);
+        JsBarcode(canvas, req.body.data);
+        canvas.toDataURL((err, data)=>{
+            console.log("barcode = " + data);
+            res.writeHead(200, {
+                "Content-Type" : "image/png",
+                "Content-Length" : data.length
+            });
+            res.end(data);
         });
-        console.log(code39);
-        console.log("asdfsadf");
+        
+
+        // let barcode = new Barc();
+        // console.log(barcode);
+        // let buf = barcode.code128(req.body.data, 400, 100);
+        // console.log(buf);
+        // res.end(buf);
+        // let code39 = barcode('code128', {
+        //     data: req.body.data,
+        //     width: 400,
+        //     height: 100,
+        // });
+        // console.log(code39);
+        // console.log("asdfsadf");
         // var outfile = path.join(__dirname, 'imgs', 'mycode.png')
         // code39.saveImage(outfile, function (err) {
         //     if (err) throw err;
         
         //     console.log('File has been written!');
         // });
+        // https://github.com/samt/barcode/issues/22#issuecomment-524492045
         // code39.getBase64((err, imagedata) =>{
         //     // console.log("error = " + err);
         //     console.log("url = " + imagedata);
--- backend/qrcode/qr.controller.ts
+++ backend/qrcode/qr.controller.ts
@@ -11,11 +11,6 @@ export default class QRController {
                 res.writeHead(500);
                 res.end("");
             }else{
-                //let data = url.replace(/.*,/,"");
-                //console.log("data = " + data);
-                //let image = new Buffer(data, "base64");
-                //console.log("image = " + image);
-        
                 res.writeHead(200, {
                     "Content-Type" : "image/png",
                     "Content-Length" : url.length
